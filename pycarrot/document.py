04fee40fe4b4ec1704072db1f57a3e5a30f1ca72
lsvih <lsvih@qq.com>
2019-04-08 10:27:15 +0800

    Add escape method to prevent xml error.

--- pycarrot/document.py
+++ pycarrot/document.py
@@ -1,8 +1,8 @@
 class Document:
     def __init__(self, title: str, url: str, content: str):
-        self.title = title
+        self.title = self.escape(title)
         self.url = url
-        self.content = content
+        self.content = self.escape(content)
 
     def __call__(self) -> str:
         return '<document>' \
@@ -10,3 +10,12 @@ class Document:
                '<title>%s</title>' \
                '<snippet>%s</snippet>' \
                '</document>' % (self.url, self.title, self.content)
+
+    @staticmethod
+    def escape(s: str) -> str:
+        s = s.replace('&', '&amp;')
+        s = s.replace('"', '&quot;')
+        s = s.replace('\'', '&apos;')
+        s = s.replace('<', '&lt;')
+        s = s.replace('>', '&gt;')
+        return s
